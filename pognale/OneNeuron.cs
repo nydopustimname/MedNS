5eb333e12dd2f0a772460a65c4c11dde54674c2a
Murk <32967078+nydopustimname@users.noreply.github.com>
2019-06-04 00:35:15 +0300

    Add files via upload

---
+++ pognale/OneNeuron.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pognale
+{
+    public class OneNeuron
+    {
+        public List<double> Weights { get;  }
+        public NeuronType NType { get; }
+        public double Output { get; private set; }
+        public OneNeuron(int inpCount, NeuronType nt=NeuronType.Normal)
+        {
+            NType = nt;
+            Weights = new List<double>();
+            for (var i=0; i<inpCount; i++)
+            {
+                Weights.Add(1);
+            }
+        }
+
+        public double FeedForward (List<double> inputs)
+        {
+            //ebnyt' proverky
+            var sum = 0.0;
+            for (var i=0; i<inputs.Count; i++)
+            {
+                sum += inputs[i] * Weights[i];
+            }
+            if (NType != NeuronType.Input)
+
+                Output = ActivationFunc(sum);
+            else
+                Output = sum;
+            return Output;
+        }
+
+        private double ActivationFunc (double x)
+        {
+            return 1.0 / (1.0 + Math.Pow(Math.E, -x));
+        }
+
+        public override string ToString()
+        {
+            return Output.ToString(); 
+        }
+
+        public void SetWeights (params double [] weights)
+        {
+            //delete after add study
+            for (var i=0; i<weights.Length; i++)
+            {
+                Weights[i] = weights[i];
+            }
+        }
+
+
+    }
+}
