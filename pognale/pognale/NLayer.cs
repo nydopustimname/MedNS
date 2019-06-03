1e4307b35ba27c3bbfeca3c5d624e63e38f77ca8
Murk <32967078+nydopustimname@users.noreply.github.com>
2019-06-04 02:16:32 +0300

    Add files via upload

---
+++ pognale/pognale/NLayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace pognale
+{
+    public class NLayer
+    {
+        public List<OneNeuron> Neurons { get; }
+        public int Count => Neurons?.Count ?? 0;
+        public NLayer(List<OneNeuron> neurons, NeuronType nt=NeuronType.Normal )
+        {
+            //ebnyt' proverky vhodnih neironov na sootv tipy
+            Neurons = neurons;
+        }
+
+        public List<double> GetSignals ()
+        {
+            var res = new List<double>();
+            foreach (var i in Neurons)
+            {
+                res.Add(i.Output);
+            }
+            return res;
+        }
+    }
+}
---
+++ pognale/pognale/Topology.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace pognale
+{
+    public class Topology
+    {
+        public int InputCount { get; }
+        public int OutputCount { get; }
+        public List<int> HiddenLayers { get; }
+        public Topology (int inpCount, int outCount, params int [] layers)
+        {
+            InputCount = inpCount;
+            OutputCount = outCount;
+            HiddenLayers = new List<int>();
+            HiddenLayers.AddRange(layers);
+        }
+    }
+}
